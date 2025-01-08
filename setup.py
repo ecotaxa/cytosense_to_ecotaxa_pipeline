e3c6a450cb7943d1d03402a3c9bb40f52b857976
Galvagno Sébastien <dev@galvagno.info>
2025-01-08 19:06:46 +0100

    include all bin folder

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
     version=version,
     packages=find_packages(),
     package_data={
-        'cytosense_to_ecotaxa_pipeline': ['bin/cyz2json*'],  # Include all cyz2json binaries
+        # 'cytosense_to_ecotaxa_pipeline': ['bin/cyz2json*'],  # Include all cyz2json binaries
+        'cytosense_to_ecotaxa_pipeline': ['bin/*'],
+
     },
     include_package_data=True,  # Ensure package_data is included
     entry_points={
